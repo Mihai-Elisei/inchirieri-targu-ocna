961bbbc35cc40dc22d22004de5d8cbcbf95af136
Mihai-Elisei <mihai.elisei@outlook.com>
2024-10-17 18:19:22 +0300

    added api service to get properties

--- frontend/app/components/properties/PropertyList.tsx
+++ frontend/app/components/properties/PropertyList.tsx
@@ -1,14 +1,35 @@
+"use client";
+import apiService from "@/app/services/apiService";
+import { useEffect, useState } from "react";
 import PropertyListItem from "./PropertyListItem";
 
+export type PropertyType = {
+  id: string;
+  title: string;
+  price_per_night: number;
+  image_url: string;
+};
+
 const PropertyList = () => {
-    return(
-        <>
-        <PropertyListItem />
-        <PropertyListItem />
-        <PropertyListItem />
-        <PropertyListItem />
-        </>
-    )
+  const [properties, setProperties] = useState<PropertyType[]>([]);
+  const getProperties = async () => {
+    const url = "/api/properties/";
+
+    const tmpProperties = await apiService.get(url);
+
+    setProperties(tmpProperties.data);
+  };
+  useEffect(() => {
+    getProperties();
+  }, []);
+
+  return (
+    <>
+      {properties.map((property) => {
+        return <PropertyListItem key={property.id} property={property} />;
+      })}
+    </>
+  );
 };
 
 export default PropertyList;
